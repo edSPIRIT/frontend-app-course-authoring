a11ecd5aa6232cbef8d87c602c1fb62ffe306a3f
milad-emami <milad.emami1025@gmail.com>
2023-07-31 14:45:35 +0330

    feat: set font for persian language

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { getLocale, injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { getConfig } from '@edx/frontend-platform';
 
 import messages from './messages';
 
-const Head = ({ intl }) => (
-  <Helmet>
-    <title>
-      {intl.formatMessage(messages['course-authoring.page.title'], { siteName: getConfig().SITE_NAME })}
-    </title>
-    <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
-  </Helmet>
-);
+const Head = ({ intl }) => {
+  const setFont = () => {
+    const body = document.querySelector('body');
+    if (getLocale() === 'fa') {
+      body.className = 'lang_fa';
+    } else if (getLocale() === 'ar') {
+      body.className = 'lang_ar';
+    } else {
+      body.removeAttribute('class');
+    }
+  };
+  useEffect(() => {
+    setFont();
+  }, [getLocale()]);
+
+  return (
+    <Helmet>
+      <title>
+        {intl.formatMessage(messages['course-authoring.page.title'], {
+          siteName: getConfig().SITE_NAME,
+        })}
+      </title>
+      <link
+        rel="shortcut icon"
+        href={getConfig().FAVICON_URL}
+        type="image/x-icon"
+      />
+    </Helmet>
+  );
+};
 
 Head.propTypes = {
   intl: intlShape.isRequired,
