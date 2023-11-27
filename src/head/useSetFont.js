affa65a8cfcceeb7895459f7793b5aed4cb60936
Khaled <info@khaledr.ir>
2023-11-27 17:14:15 +0330

    fix: fix lang code

--- src/head/useSetFont.js
+++ src/head/useSetFont.js
@@ -5,7 +5,7 @@ const useSetFont = () => {
   useEffect(() => {
     const setFont = () => {
       const body = document.querySelector('body');
-      if (getLocale() === 'fa') {
+      if (getLocale() === 'fa' || getLocale() === 'fa-ir') {
         body.className = 'lang_fa';
       } else if (getLocale() === 'ar') {
         body.className = 'lang_ar';
