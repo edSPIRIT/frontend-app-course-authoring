9e5339fc446acf2164ed5060372a0682a9f0a649
Khaled <info@khaledr.ir>
2023-11-27 17:02:39 +0330

    feat: add setFont hook for persian font

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -1,39 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
-import { getLocale, injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { getConfig } from '@edx/frontend-platform';
 
 import messages from './messages';
+import useSetFont from './useSetFont';
 
 const Head = ({ intl }) => {
-  const setFont = () => {
-    const body = document.querySelector('body');
-    if (getLocale() === 'fa') {
-      body.className = 'lang_fa';
-    } else if (getLocale() === 'ar') {
-      body.className = 'lang_ar';
-    } else {
-      body.removeAttribute('class');
-    }
-  };
-  useEffect(() => {
-    setFont();
-  }, [getLocale()]);
-
-  return (
-    <Helmet>
-      <title>
-        {intl.formatMessage(messages['course-authoring.page.title'], {
-          siteName: getConfig().SITE_NAME,
-        })}
-      </title>
-      <link
-        rel="shortcut icon"
-        href={getConfig().FAVICON_URL}
-        type="image/x-icon"
-      />
-    </Helmet>
-  );
+  useSetFont();
+return (
+  <Helmet>
+    <title>
+      {intl.formatMessage(messages['course-authoring.page.title'], { siteName: getConfig().SITE_NAME })}
+    </title>
+    <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
+  </Helmet>
+);
 };
 
 Head.propTypes = {
